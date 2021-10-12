e5747d5420cdb90e021e94571223e5dd25ea2c53
INeedEstus <medcome.lan@gmail.com>
2021-10-12 15:06:36 +0200

    lab2

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,18 +14,35 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "Hello there! :)";
 
+    private FragmentManager fragmentManager;
+    private Fragment fragment1, fragment2, fragment3, fragment4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+        fragment4 = new Fragment4();
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.Frame1, fragment1);
+        transaction.add(R.id.Frame2, fragment2);
+        transaction.add(R.id.Frame3, fragment3);
+        transaction.add(R.id.Frame4, fragment4);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
+    /*
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-    }
+    }*/
 }
 
